5cc2f0fd69def29730a9cdb9bcfc72bb3ae395c5
Jens Schuppe <schuppe@systopia.de>
2022-07-12 12:57:58 +0200

    Upgrade to Civix 22.05.2

--- selfservice.php
+++ selfservice.php
@@ -45,7 +45,6 @@ function selfservice_civicrm_tokenValues(&$values, $cids, $job = null, $tokens =
   CRM_Selfservice_HashLinks::tokenValues($values, $cids, $job, $tokens, $context);
 }
 
-
 /**
  * Set permissions for runner/engine API call
  */
@@ -88,15 +87,6 @@ function selfservice_civicrm_permission(&$permissions) {
  */
 function selfservice_civicrm_config(&$config) {
   _selfservice_civix_civicrm_config($config);
-}
-
-/**
- * Implements hook_civicrm_xmlMenu().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_xmlMenu
- */
-function selfservice_civicrm_xmlMenu(&$files) {
-  _selfservice_civix_civicrm_xmlMenu($files);
 }
 
 /**
@@ -154,54 +144,6 @@ function selfservice_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
 }
 
 /**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_managed
- */
-function selfservice_civicrm_managed(&$entities) {
-  _selfservice_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_caseTypes
- */
-function selfservice_civicrm_caseTypes(&$caseTypes) {
-  _selfservice_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_angularModules
- */
-function selfservice_civicrm_angularModules(&$angularModules) {
-  _selfservice_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_alterSettingsFolders
- */
-function selfservice_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _selfservice_civix_civicrm_alterSettingsFolders($metaDataFolders);
-}
-
-/**
  * Implements hook_civicrm_entityTypes().
  *
  * Declare entity types provided by this module.
@@ -212,13 +154,6 @@ function selfservice_civicrm_entityTypes(&$entityTypes) {
   _selfservice_civix_civicrm_entityTypes($entityTypes);
 }
 
-/**
- * Implements hook_civicrm_thems().
- */
-function selfservice_civicrm_themes(&$themes) {
-  _selfservice_civix_civicrm_themes($themes);
-}
-
 // --- Functions below this ship commented out. Uncomment as required. ---
 
 /**
@@ -226,9 +161,8 @@ function selfservice_civicrm_themes(&$themes) {
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_preProcess
  *
-function selfservice_civicrm_preProcess($formName, &$form) {
 
-} // */
+ // */
 
 /**
  * Implements hook_civicrm_navigationMenu().
